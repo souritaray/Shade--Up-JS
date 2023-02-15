be7a328579c5fca4582bb2e3ded45ec8a3827f60
Sourita Ray <souritaray@gmail.com>
2023-02-15 18:24:50 +0530

    fixed prototype

--- js/main.js
+++ js/main.js
@@ -1,66 +1,58 @@
 function randompickerbtn() {
-    let clr="#";
-    for(let i=0;i<6;i++){
-        clr=clr+Math.floor(Math.random()*9);
-    }
-    document.getElementById("randomPicker").style.backgroundColor=clr;
-    document.getElementById("randomValuePara").innerText=clr;
-  
+  let clr = "#";
+  for (let i = 0; i < 6; i++) {
+    clr = clr + Math.floor(Math.random() * 9);
+  }
+  document.getElementById("randomPicker").style.backgroundColor = clr;
+  document.getElementById("randomValuePara").innerText = clr;
 }
 
 function hexFindbtn() {
-        let hexVal=document.querySelector("#hexInput").value;
-        document.querySelector("#color-box").style.backgroundColor=hexVal;
-
-    
+  let hexVal = document.querySelector("#hexInput").value;
+  document.querySelector("#color-box").style.backgroundColor = hexVal;
 }
 
 function RGBFindbtn() {
-    let r=document.getElementById("RInput").value;
-    let b=document.getElementById ("BInput").value;
-    let g=document.getElementById("GInput").value;
-    document.querySelector("#rgb-color-box").style.backgroundColor=`rgb(`+r+`,`+g+`,`+b+`)`;
-  
+  let r = document.getElementById("RInput").value;
+  let b = document.getElementById("BInput").value;
+  let g = document.getElementById("GInput").value;
+  document.querySelector("#rgb-color-box").style.backgroundColor =
+    `rgb(` + r + `,` + g + `,` + b + `)`;
 }
 
 function RangeSelector() {
-    let r=document.getElementById("RrInput").value;
-    let b=document.getElementById ("BrInput").value;
-    let g=document.getElementById("GrInput").value;
-    document.getElementById("range-color-box").style.backgroundColor='rgb('+ r +','+ g +','+ b + ')';
-  
+  let r = document.getElementById("RrInput").value;
+  let b = document.getElementById("BrInput").value;
+  let g = document.getElementById("GrInput").value;
+  document.getElementById("range-color-box").style.backgroundColor =
+    "rgb(" + r + "," + g + "," + b + ")";
 }
 
 // hex to rgb converter
 
-String.prototype.convertToRGB = function () {
-let hexVal=document.querySelector("input#hexCInp").value;
-var red=parseInt(hexVal[0]+hexVal[1],16);
-var green=parseInt(hexVal[2]+hexVal[3],16);
-var blue=parseInt(hexVal[4]+hexVal[5],16);
-document.querySelector("#hexCOut").textContent= ` rgb(${red} , ${green} , ${blue})`;
+String.prototype.convertToRGB = function (hexVal) {
+  var red = parseInt(hexVal[0] + hexVal[1], 16);
+  var green = parseInt(hexVal[2] + hexVal[3], 16);
+  var blue = parseInt(hexVal[4] + hexVal[5], 16);
+  return ` rgb(${red} , ${green} , ${blue})`;
 };
 
 // rgb to hex converter
 
-String.prototype.convertToHex = function () {
-    let r=Number(document.querySelector("input#RCInput").value);
-    let g=Number(document.querySelector("input#GCInput").value);
-    let b=Number(document.querySelector("input#BCInput").value);
-    
-    document.querySelector("#rgbCOut").textContent="#" + ColorToHex(r) + ColorToHex(g) + ColorToHex(b);
-    };
-    function ColorToHex(color){
-        let hexClr=color.toString(16);
-    return hexClr.length==1?"0"+hexClr:hexClr;
-    }
-    
+String.prototype.convertToHex = function (color) {
+  let hexClr = Number(color).toString(16);
+  return hexClr.length === 1 ? "0" + hexClr : hexClr;
+};
 
 function hexConvbtn() {
-    String.prototype.convertToRGB()
-  
+  let hexVal = document.querySelector("input#hexCInp").value;
+  document.querySelector("#hexCOut").textContent = hexVal.convertToRGB(hexVal);
 }
 
 function rgbConvbtn() {
-  String.prototype.convertToHex()
+  let r = document.querySelector("input#RCInput").value;
+  let g = document.querySelector("input#GCInput").value;
+  let b = document.querySelector("input#BCInput").value;
+  document.querySelector("#rgbCOut").textContent =
+    "#" + r.convertToHex(r) + g.convertToHex(g) + b.convertToHex(b);
 }
